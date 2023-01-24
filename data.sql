469e17fbaea41ef0512f9290cddb1beeadd33fd9
Asha S V <ashasvijayan@gmail.com>
2023-01-24 16:05:46 +0400

    Update data.sql

--- data.sql
+++ data.sql
@@ -6,3 +6,16 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
     ('Pikachu', '2021-01-07', 1, 'FALSE', 15.04),
     ('Devimon', '2017-05-12', 5, 'TRUE', 11)
 ;
+
+/* Add more data to database */
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES 
+        ('Devimon', '2017-05-12', 5, 'TRUE', 11), 
+        ('Charmander', '2022-02-08', 0, 'FALSE', -11),
+        ('Plantmon', '2021-11-15', 2, 'TRUE', -5.7), 
+        ('Squirtle', '1993-04-02', 3, 'FALSE', -12.13), 
+        ('Angemon', '2005-06-12', 1, 'TRUE', -45),
+        ('Boarmon', '2005-06-07', 7, 'TRUE', 20.4), 
+        ('Blossom', '1998-08-13', 3, 'TRUE', 17), 
+        ('Ditto', '2022-05-14', 4, 'TRUE', 22)
+;
